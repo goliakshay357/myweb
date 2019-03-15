8baa6340ae418a70b67e316450490ae1ad1dfed8
goliakshay357 <32712438+goliakshay357@users.noreply.github.com>
2019-03-15 19:54:19 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@
 $(document).ready(function(){
     $('.header').height($(window).height());
    })
+
+$(document).ready(function(){
+    $('.overlayabout').height($('.about').height());
+   })
+
+      
 $(".navbar a").click(function(){
  $("body,html").animate({
   scrollTop:$("#" + $(this).data('value')).offset().top
