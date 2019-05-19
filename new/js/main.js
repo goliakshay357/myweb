5cc3530a5ec75936987f27d37741400d9ece004d
goliakshay357 <32712438+goliakshay357@users.noreply.github.com>
2019-05-19 22:39:03 +0530

    Add files via upload

---
+++ new/js/main.js
@@ -0,0 +1,17 @@
+
+// this is for making height of window in window size!!!
+$(document).ready(function(){
+    $('.header').height($(window).height());
+   })
+
+// $(document).ready(function(){
+//     $('.overlayabout').height($('.about').height());
+//    })
+
+      
+$(".navbar a").click(function(){
+ $("body,html").animate({
+  scrollTop:$("#" + $(this).data('value')).offset().top
+ },1000)
+ 
+})   
